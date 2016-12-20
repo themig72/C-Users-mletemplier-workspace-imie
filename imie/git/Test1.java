341823f8e2dd7e89f6f63c5d46a1a5c0ef82a1b4
dcossedu <dcossedu@LMT15110004.imie.lan>
2016-12-20 10:09:44 +0100

    rebelotte

--- imie/git/Test1.java
+++ imie/git/Test1.java
@@ -1,10 +1,13 @@
 package imie.git;
 
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
 public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Salut les noobs !!!!" );
+		System.out.println("On va y arriver !!! BDM :-)");
 	}
 
 }
